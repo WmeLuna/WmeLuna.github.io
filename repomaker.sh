b89768090d51a480d1faeabc5c132e565c4fb4b8
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2021-06-07 13:58:47 -0400

    Update repomaker.sh

--- repomaker.sh
+++ repomaker.sh
@@ -1,6 +1,7 @@
+sudo apt update
 sudo apt install python3-pip python3-wheel python3-dev virtualenv build-essential -y
 sudo apt install fdroidserver libmagic1 rsync git python3-pyqt5.qtwebengine python3-pyqt5.qtwebkit -y
 clear
-yes | pip install django fdroidserver html5lib
-yes | pip install repomaker[gui]
+yes | pip3 install django fdroidserver html5lib
+yes | pip3 install repomaker[gui]
 repomaker-server
