7ace1d60bf702160d3a6565ed9d98b63c1e436e3
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2022-02-07 13:21:34 -0500

    Update brave.ps1

--- sc/brave.ps1
+++ sc/brave.ps1
@@ -1,20 +1,22 @@
+cd $env:userprofile\Downloads
+
 $repo = "brave/brave-browser"
 $file = "BraveBrowserStandaloneSilentSetup.exe"
+$localappdata = $env:LOCALAPPDATA
+$tag = "v1.35.100"
 
-$releases = "https://api.github.com/repos/$repo/releases"
-
-
+#$releases = "https://api.github.com/repos/$repo/releases"
 #Write-Host Determining latest release
 #$tag = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].tag_name
-
-$tag = "v1.34.80"
 
 $download = "https://github.com/$repo/releases/download/$tag/$file"
 $name = $file.Split(".")[0]
 $exeFile = "$name.exe"
 
-Write-Host Dowloading latest release
+Write-Host Dowloading Brave
 Invoke-WebRequest $download -Out $exeFile
 
 start $exeFile
-pause
+Write-Host Installing Brave
+while (!(Test-Path "$localappdata\BraveSoftware\Brave-Browser\Application\brave.exe")) { Start-Sleep 10 }
+exit
